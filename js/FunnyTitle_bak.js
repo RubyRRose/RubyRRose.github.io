7e671512ea473a659be929a06f1c954d8a1298a6
RubyRRose <2849027391@qq.com>
2023-01-12 19:48:56 +0800

    Site updated: 2023-01-12 19:48:55

--- js/FunnyTitle_bak.js
+++ js/FunnyTitle_bak.js
@@ -3,10 +3,12 @@
  var titleTime;
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
          document.title = "网页崩溃了ヽ(●-`Д´-)ノ";
          clearTimeout(titleTime);
      }
      else {
+         $('[rel="icon"]').attr('href', "/img/trhx2.png");
          document.title = "我开玩笑的ヾ(Ő∀Ő3)ノ" + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
